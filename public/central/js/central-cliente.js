0f7962b6e7aa42749d49f344ee3443221e31402c
anetfibraoptica-sys <anetfibraoptica@gmail.com>
2026-07-26 17:21:12 -0300

    Delete public directory

--- public/central/js/central-cliente.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('dados').innerText='Área do assinante criada.';
--- public/cliente/cliente.js
+++
@@ -1,9 +0,0 @@
-function entrar(){
-  const login = document.getElementById('login').value;
-  const senha = document.getElementById('senha').value;
-
-  if(login && senha){
-    document.getElementById('msg').innerText =
-      'Estrutura da Central criada. Próxima etapa: conectar ao Supabase.';
-  }
-}
